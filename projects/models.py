55c1fc8c5afd2b0aa54df8a30a7445ae8f1d1fee
prygrn <pierremogrison@gmail.com>
2024-01-28 23:55:44 +0100

    Creating 1st app and Working with db

---
+++ projects/models.py
@@ -0,0 +1,9 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FilePathField(path="/projects/img")
